8016850ae935bfb08ea83c96b070e2295b69a32a
joeflow <thejoeflow@gmail.com>
2020-04-04 11:43:02 -0400

    Made read transactions not have to submit to the orderer

--- chaincode/contracts/principal/visitorContract.ts
+++ chaincode/contracts/principal/visitorContract.ts
@@ -1,9 +1,4 @@
 import Visitor from '../../domain/visitor'
 import {AbstractPrincipalContract} from './abstractPrincipalContract'
 
-export class VisitorContract extends AbstractPrincipalContract<Visitor> {
-
-    constructor() {
-        super('visitor', Visitor)
-    }
-}
+export class VisitorContract extends AbstractPrincipalContract<Visitor> {constructor() {super('visitor', Visitor)}}
